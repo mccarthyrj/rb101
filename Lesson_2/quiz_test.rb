192857c4a3e56063c2bf4cb52153ac0d9890cb3c
RJ McCarthy <rjmccarthy92@gmail.com>
2023-08-17 18:30:43 -0400

    Updated directory names.

---
+++ Lesson_2/quiz_test.rb
@@ -0,0 +1,12 @@
+# quiz worksheet
+
+name = "Lisa"
+
+def name
+  "George"
+end
+
+loop do
+  puts name()
+  break
+end
---
+++ Lesson_2/rps_bonuses.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# rps_bonuses.rb
+
+VALID_CHOICES = %w[rock paper scissors lizard spock].freeze
+
+def prompt(message)
+  puts "=> #{message}"
+end
+
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'spock' && second == 'scissors') ||
+    (first == 'spock' && second =='rock') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'lizard' && second == 'spock')
+end
+
+def display_results(player, computer)
+  if win?(player, computer)
+    prompt('You won!')
+  elsif win?(computer, player)
+    prompt('Computer won!')
+  else
+    prompt("It's a tie!")
+  end
+end
+
+
+loop do
+  choice = ''
+  loop do
+    prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
+    choice = gets.chomp
+
+    break if VALID_CHOICES.include?(choice)
+
+    prompt("That's not a valid choice.")
+  end
+
+  computer_choice = VALID_CHOICES.sample
+  puts "You chose: #{choice}; Computer chose: #{computer_choice}"
+
+  display_results(choice, computer_choice)
+
+  prompt('Do you want to play again?')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt('Thanks for playing!')
